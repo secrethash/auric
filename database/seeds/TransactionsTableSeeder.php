deacdce5763aad2dff55c6a4e64d868c86bf04af
Shashwat Mishra <secrethash96@gmail.com>
2020-07-14 14:04:05 +0530

    Cleared Transactions Table Seeder

--- database/seeds/TransactionsTableSeeder.php
+++ database/seeds/TransactionsTableSeeder.php
@@ -12,27 +12,11 @@ class TransactionsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('transactions')->delete();
-        
-        \DB::table('transactions')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
-                'sign' => '5353rrw5553536',
-                'note' => 'Invested',
-                'amount' => 300,
-                'status' => 'success',
-                'payment_id' => 'pay_uteydfs726525',
-                'request_id' => 'req_yetrsg62834362',
-                'user_id' => 1,
-                'order_id' => 2,
-                'created_at' => '2020-06-30 13:45:07',
-                'updated_at' => '2020-06-30 13:45:07',
-            ),
-        ));
-        
-        
+
+
+
     }
 }
